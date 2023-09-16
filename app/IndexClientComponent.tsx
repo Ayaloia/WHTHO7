5838531801c63e39439a99c45bcce8a13ebfc777
aldlss <ayaldlss@gmail.com>
2023-09-16 20:19:12 +0800

    fix: 修复了 SkewedCard 的一点小 Bug
    但是好像还是有些问题，后面再看看（

--- app/IndexClientComponent.tsx
+++ app/IndexClientComponent.tsx
@@ -6,7 +6,7 @@ import { useMouseHovered } from "react-use";
 export function SkewedCard({
     children,
     className,
-    rotateMultiple = 4,
+    rotateMultiple = 2,
 }: {
     children: React.ReactNode;
     className?: string | undefined;
@@ -18,10 +18,9 @@ export function SkewedCard({
         // 参考了 GitHub 官网的实现，不过这里重复用了 while/when Hover，可能会有性能问题
         <motion.div
             ref={ref}
-            className={`w-full h-full border-gray rounded-xl bg-card-bg border-1 p-4 overflow-hidden ${className}`}
+            className={`w-full h-full border-gray rounded-xl bg-card-bg border-1 p-4 overflow-hidden transform-origin-center ${className}`}
             initial={{ transform: "perspective(700px) rotateX(0) rotateY(0)" }}
             whileHover={{
-                transformOrigin: "center",
                 transform: `perspective(700px) rotateX(${
                     (elY / elH - 0.5) * rotateMultiple
                 }deg) rotateY(${(elX / elW - 0.5) * -rotateMultiple}deg)`,
